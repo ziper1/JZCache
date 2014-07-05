14b52166f105791ad9a7945f69d91a13bd353f27
Jacek Zapart <ziperr@gmail.com>
2014-07-05 15:28:34 +0200

    Initial commit: project files

---
+++ ExampleJZCache/JZCache/JZCache.swift
@@ -0,0 +1,9 @@
+//
+//  JZCache.swift
+//  ExampleJZCache
+//
+//  Created by Jacek Zapart on 05.07.2014.
+//  Copyright (c) 2014 Jacek Zapart. All rights reserved.
+//
+
+import Foundation
---
+++ ExampleJZCache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleJZCache
+//
+//  Created by Jacek Zapart on 05.07.2014.
+//  Copyright (c) 2014 Jacek Zapart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
