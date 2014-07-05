b1b07bc7aa407b082ce0fd952e97aa1000dcf70d
Jacek Zapart <ziperr@gmail.com>
2014-07-05 21:03:11 +0200

    added cache in Dictionary

--- ExampleJZCache/JZCache/JZCache.swift
+++ ExampleJZCache/JZCache/JZCache.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+
+class JZCache
+{
+    let store = NSCache();
+    var alternativeStore: Dictionary<String, Any> = Dictionary<String, Any>()
+    var lastUpdate: Dictionary<String, NSDate> = Dictionary<String, NSDate>()
+    var maxTime: NSTimeInterval?
+
+    init(){
+        self.maxTime = NSTimeInterval(300)
+    }
+    
+    init(invalidateAfter: NSTimeInterval){
+        self.maxTime = invalidateAfter
+    }
+    
+    func cache <T, S> (function: (T -> S), params: T ) -> S {
+        let key:String = self.createKey(function, params: params)
+        
+        // read
+//        let cachedValue = self.read(key)
+        // alternative
+        let stored: (cachedValue: Any?, shouldInvalidate: Bool) = read(key)
+        if stored.cachedValue != nil && stored.shouldInvalidate == false
+        {
+            println("returned cached value: " + toString(stored.cachedValue))
+            return stored.cachedValue as S
+        }
+        
+        let result = function(params)
+        
+        // save result
+        self.save(result, forKey:key)
+        
+        return result
+    }
+    
+    func createKey<T, S> (function: (T -> S), params: T ) -> String {
+        println("Key: " + toString(params))
+        // TODO: do some magic here to pass function name
+        return toString(params)
+    }
+    
+    @objc func shouldInvalidate (key: String) -> Bool
+    {
+        let last = lastUpdate[key]
+        
+        if (last){
+            let interval = NSDate().timeIntervalSinceDate(last)
+            if (interval > maxTime){
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func save(value: Any, forKey: String)
+    {
+        println("save to cache: " + toString(value))
+        alternativeStore.updateValue(value, forKey: forKey)
+        lastUpdate.updateValue(NSDate(), forKey: forKey)
+    }
+    
+    func read(key: String) -> (Any?, Bool)
+    {
+        return (alternativeStore[key], shouldInvalidate(key))
+    }
+    
+    // some problems with casting type S to AnyObject and calling the NSCache methods
+//    @objc func read(key: AnyObject) -> AnyObject {
+//        return store.objectForKey(key) // use tuple function, params as key
+//    }
+//    
+//    @objc func save(value: AnyObject, forKey: AnyObject) {
+//        store.setObject(value, forKey: forKey) // use tuple function, params as key
+//    }
+    
+    
+}
--- ExampleJZCache/ViewController.swift
+++ ExampleJZCache/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    // sample function operates on Ints
+    func intfunc (a: Int, b: Int) -> Int{
+        sleep(1)
+        return a+b
+    }
+    
+    // sample function operates on Strings
+    func stringfunc (a: String, b: String, c:String) -> String{
+        return a+b+c
+    }
+   
+    func makeIncrementor(forIncrement amount: Int) -> () -> Int {
+        var runningTotal = 0
+        func incrementor() -> Int {
+            runningTotal += amount
+            return runningTotal
+        }
+        return incrementor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // some examples
+        let cache = JZCache()
+        let result = cache.cache (intfunc, params: (3, 5))
+        let bresult = cache.cache (stringfunc, params: ("aaa", "bbb", "ccc"))
+        let cresult = cache.cache (makeIncrementor, params: 3)
+        let result2 = cache.cache (intfunc, params: (3, 5))
     }
 
     override func didReceiveMemoryWarning() {
